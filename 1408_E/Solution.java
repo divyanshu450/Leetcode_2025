3ff1eb8105e29f82e5231863e347f8e6dae84809
divyanshu450 <divyanshu.kumar450@gmail.com>
2025-01-07 14:05:03 +0530

    07/01/2025_Question 1408

---
+++ 1408_E/Solution.java
@@ -0,0 +1,48 @@
+import java.util.*;// importing package to run in local..
+
+public class Solution {
+    public static void main(String args[]){
+        
+        String[] words = {"mass","as","hero","superhero"};
+        System.out.println("INPUT: "+Arrays.toString(words));
+        List<String> result = stringMatching(words);
+        System.out.println("OUTPUT: "+result.toString());
+
+        //uncomment below lines to see each value in result list...
+        /*  
+        Iterator<String> itr = result.iterator();
+        while (itr.hasNext()) {
+            String word = itr.next();
+            System.out.println(word);
+        }
+        */
+
+    }
+
+//TC: O(n log n + n^2 * k) where n is the number of words and k is the average length of the strings. Also O(n log n) is from Arrays.sort()
+//SC: O(n + k)
+    public static List<String> stringMatching(String[] words) {
+        List<String> list = new ArrayList<String>();
+        Arrays.sort( words, new Comparator<String>() {
+             @Override
+             public int compare(String s1, String s2){
+                return Integer.compare(s1.length(), s2.length());
+             }
+        });
+        for(int i = 0; i < words.length; i++){
+            String word = words[i];
+            for(int j = i+1; j <words.length; j++){
+                String[] checkArr = words[j].split(word);
+                if(checkArr.length > 0 && checkArr[0] == words[j]){//no split
+                    continue;
+                }
+                if(checkArr.length > 0 && checkArr[0] != words[j])//means available
+                {
+                    list.add(word);
+                    break;
+                }
+            }
+        }
+        return list;
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 | S.No | LeetCode S.No. | Title                                                                 |
 |------|----------------|----------------------------------------------------------------------|
 | 1    | [1769](https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/)           | [Minimum Number of Operations to Move All Balls to Each Box](./1769_M)          |
+| 2    | [1408](https://leetcode.com/problems/string-matching-in-an-array/)    | [String Matching in an Array](./1408_E)
